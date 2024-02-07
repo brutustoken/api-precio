375a31f1f3c702b570dd8304e66dcac237c6622d
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2024-02-07 15:05:23 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ const CIRC_BRUT = process.env.APP_GOOGLE_API_CIRC_BRUT
 const WALLET_SR = "TWVVi4x2QNhRJyhqa7qrwM4aSXnXoUDDwY";
 const TRONGRID_API = "https://api.trongrid.io";
 const addressContract = process.env.APP_CONTRACT || "TBRVNF2YCJYGREKuPKaP7jYYP9R1jvVQeq";
-const addressContractPool = process.env.APP_CONTRACT_POOL || "TMzxRLeBwfhm8miqm5v2qPw3P8rVZUa3x6";
 const addressContractBrst = process.env.APP_CONTRACT_BRST || "TF8YgHqnJdWzCbUyouje3RYrdDKJYpGfB3";
 
 const addressContractPoolProxy = process.env.APP_CONTRACT_POOL_PROXY || "TRSWzPDgkEothRpgexJv7Ewsqo66PCqQ55";
@@ -94,13 +93,6 @@ let contract_POOL_PROXY = {}
 contract_POOL_PROXY = tronWeb3.contract(abi_POOLBRST,addressContractPoolProxy)
 
 
-let contract_Pool = {}
-tronWeb3.contract().at(addressContractPool)
-.then((r)=>{
-	contract_Pool = r
-	console.log("pool v3 conected")
-})
-
 
 var inicio = new CronJob('0 */1 * * * *', async() => {
 	console.log('-----------------------------------');
@@ -146,14 +138,11 @@ if(develop === "false"){
 	//minutos.start();
 
 }else{
-	
-}
+	/// colocar funciones para probar solo en entorno de pruebas
 
 
-async function datosBrut() {
-	let precio = await fetch(API).then((r)=>{return r.json()}).catch(error =>{console.error(error)})
-	return precio;
 
+	///
 }
 
 async function guardarDatos(temp){
@@ -189,144 +178,40 @@ async function guardarDatos(temp){
 
 async function retirarTrxContrato() {
 
-	var cuenta = await tronWeb.trx.getAccount(addressContractPool);
+	var cuenta = await tronWeb.trx.getAccount(addressContractPoolProxy);
+
+	let trxSolicitado = await contract_POOL_PROXY.TRON_SOLICITADO().call();
+	if(trxSolicitado._hex)trxSolicitado = parseInt(trxSolicitado._hex);
+	trxSolicitado = new BigNumber(trxSolicitado);
+
+	var descongelando = await tronWeb.trx.getCanWithdrawUnfreezeAmount("TWVVi4x2QNhRJyhqa7qrwM4aSXnXoUDDwY",Date.now()+14*86400*1000)
+    if(descongelando.amount){
+		trxSolicitado = trxSolicitado.minus(descongelando.amount)
+	}
 	
-	let contract = await tronWeb.contract().at(addressContractPool); //TRX_BRST
+	var RR = await contract_POOL_PROXY.TRON_PAY_BALANCE_FAST().call();
+	if(RR._hex)RR = parseInt(RR._hex)
+	RR=new BigNumber(RR)
 
-	let trxSolicitado = await contract.TRON_SOLICITADO().call();
-	trxSolicitado = parseInt(trxSolicitado._hex);
+	var WhiteList = await contract_POOL_PROXY.TRON_PAY_BALANCE_WHITE().call();
+	if(WhiteList._hex)WhiteList=parseInt(WhiteList._hex)
+	WhiteList = new BigNumber(WhiteList)
 
-	trxSolicitado = new BigNumber(trxSolicitado)
 	var balance = new BigNumber(cuenta.balance)
 
-	var trxRemanente = 1*10**6;
+	trxSolicitado = trxSolicitado.plus(1*10**6).plus(RR).plus(WhiteList)
+	
 
-	trxSolicitado = trxSolicitado.plus(trxRemanente)
+	if(balance.gt(trxSolicitado) && true){
+		console.log("trx en contract: "+balance.shiftedBy(-6).toString(10))
+		console.log("Para retirar: "+trxSolicitado.shiftedBy(-6).toString(10))
+		console.log("diferencia: "+balance.minus(trxSolicitado).shiftedBy(-6).toString(10))
 
-	if(balance.gt(trxSolicitado)){
-		var tx = await contract.redimTRX(balance.minus(trxSolicitado).toString(10)).send();
+		var tx = await contract_POOL_PROXY.redimTRX(balance.minus(trxSolicitado).toString(10)).send();
 		console.log("https://tronscan.io/#/transaction/"+tx)
 	}
 
 }
-
-async function upDatePrecio(){
-
-	var proximoCiclo = await tronWeb.trx.timeUntilNextVoteCycle();
-	proximoCiclo = new Date(proximoCiclo*1000).toLocaleString();
-
-	var cuenta = await tronWeb.trx.getAccount();
-
-	var wallet = tronWeb.address.fromHex(cuenta.address);
-	console.log("UPDATING PRICE");
-
-	console.log("Wallet: "+wallet);
-
-	cuenta.balance = cuenta.balance/10**6;
-
-	//console.log(cuenta);
-
-	var votos = 0;
-
-	if(cuenta.account_resource.frozen_balance_for_energy.frozen_balance){
-		votos += cuenta.account_resource.frozen_balance_for_energy.frozen_balance;
-	}
-	if(cuenta.account_resource.delegated_frozen_balance_for_energy){
-		votos += cuenta.account_resource.delegated_frozen_balance_for_energy;
-	}
-		
-	if(cuenta.frozen[0].frozen_balance){
-		votos += cuenta.frozen[0].frozen_balance;
-	}
-		
-	if(cuenta.delegated_frozen_balance_for_bandwidth){
-		votos += cuenta.delegated_frozen_balance_for_bandwidth;
-	}
-		
-	votos = parseInt(votos/10**6)
-
-
-	var recompensas = await tronWeb.trx.getReward(wallet);
-	recompensas = recompensas/10**6;
-
-	var ultimoretiro = new Date(cuenta.latest_withdraw_time).toLocaleString();
-
-	var hoy = Date.now();
-
-	var proximoretiro = new Date(cuenta.latest_withdraw_time+86400000).toLocaleString();
-	console.log("---------DISPONIBLE-----------");
-	console.log("Recompensas: "+recompensas+" TRX");
-	console.log("Saldo: "+cuenta.balance+" TRX");
-	if(cuenta.votes){
-		votos = votos-cuenta.votes[0].vote_count;
-		console.log("Votos: "+cuenta.votes[0].vote_count);
-
-	}
-	console.log("Nuevos Votos: "+votos);
-	console.log("---------TIEMPOS-----------");
-	console.log("Ultimo retiro: "+ultimoretiro);
-	console.log("Proximo retiro: "+proximoretiro);
-	console.log("Proximo Ciclo: "+proximoCiclo);
-	console.log("---------EJECUCIÓN-----------");
-
-	// ajusta la caticad de TRX para que siempre sea igual en el contrato
-	if(recompensas > 0 && false){
-		const contractPool = await tronWeb.contract().at(addressPool);
-		var tx = await contractPool.gananciaDirecta(parseInt(recompensas/10**6)).send();
-		console.log("[Ejecución Contrato: "+tx+"]");
-	}
-	
-	// Reclamar recompensas cada dia y asignarlo a las ganancias
-	if (false && hoy > cuenta.latest_withdraw_time+86400000 && recompensas > 0) {
-		console.log("[Reclamando recompensa: "+(hoy > cuenta.latest_withdraw_time+86400000)+"]");
-		const tradeobj = await tronWeb.transactionBuilder.withdrawBlockRewards(cuenta.address, 1);
-		const signedtxn = await tronWeb.trx.sign(tradeobj, PERKSA);
-		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-		console.log("[Transaccion: "+receipt.txid+"]");
-
-		if(false){
-			const contractPool = await tronWeb.contract().at(addressPool);
-			var tx = await contractPool.gananciaDirecta(tronWeb.toSun(recompensas)).send();
-			console.log("[Ganancia Directa Contrato: "+tx+"]");
-		}
-		delay(3000);
-	}
-	// actualizar balance para congelar todo el disponible
-	var cuenta = await tronWeb.trx.getAccount();
-	cuenta.balance = cuenta.balance/10**6;
-
-	// se congela todo el saldo a la cuenta principal por energia
-	if (false && cuenta.balance > 1) {
-		console.log("[Congelando disponible: "+(parseInt(cuenta.balance))+" => "+(cuenta.balance > 1)+"]");
-		var toFreez = tronWeb.toSun(parseInt(cuenta.balance));
-		const tradeobj = await tronWeb.transactionBuilder.freezeBalance(toFreez, 3, "ENERGY", cuenta.address, addressParaenergia, 1)
-		const signedtxn = await tronWeb.trx.sign(tradeobj, PEKEY);
-		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-		console.log("[Transaccion: "+receipt.txid+"]");
-		delay(3000);
-	}
-	
-	var cuenta = await tronWeb.trx.getAccount();
-	cuenta.balance = cuenta.balance/10**6;
-
-	var votos = (cuenta.account_resource.frozen_balance_for_energy.frozen_balance+cuenta.account_resource.delegated_frozen_balance_for_energy+cuenta.frozen[0].frozen_balance+cuenta.delegated_frozen_balance_for_bandwidth)/10**6;
-	// se verifica si hay votos nuevos y se vota con todos los votos al SR
-	votos = parseInt(votos);
-	var votar = votos;
-	if(cuenta.votes){
-		votos = votos-cuenta.votes[0].vote_count;
-	}
-
-	if (true && votos > 1) {
-		console.log("[+"+(votos)+" votos para SR: "+(votos > 1)+"]");
-		console.log("asignar: "+votar)
-		const tradeobj = await tronWeb.transactionBuilder.vote({'TWVVi4x2QNhRJyhqa7qrwM4aSXnXoUDDwY':votar}, wallet, 1);
-		const signedtxn = await tronWeb.trx.sign(tradeobj, PERKSA);
-		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-		console.log("[Transaccion: "+receipt.txid+"]");
-	}
-
-};
 
 async function comprarBRST(){
 
@@ -339,8 +224,6 @@ async function comprarBRST(){
 
 	cuenta.wallet = tronWeb2.address.fromHex(cuenta.address);
 
-	const contractPool = await tronWeb2.contract().at(addressContractPool);
-
 	console.log("--------- AUTOCOMPRA BRST -----------");
 	console.log("wallet: "+cuenta.wallet);
 	console.log("balance: "+cuenta.balance+" TRX");
@@ -350,167 +233,10 @@ async function comprarBRST(){
 	// comprar auto brst
 	if(cuenta.balance >= 100 && true){
 		
-		var tx = await contractPool.staking().send({callValue: parseInt(cuenta.balance*10**6)});
+		var tx = await contract_POOL_PROXY.staking().send({callValue: parseInt(cuenta.balance*10**6)});
 		console.log("[Ejecución: compra de BRST "+tx+"]");
 	}
 	
-
-};
-
-async function ajusteMoneda(){
-
-	var cuenta = await tronWeb.trx.getAccount();
-	cuenta.balance = cuenta.balance/10**6;
-	cuenta.wallet = tronWeb.address.fromHex(cuenta.address);
-
-	var recompensas = await tronWeb.trx.getReward(cuenta.address);
-	recompensas = recompensas/10**6;
-
-	if (true && Date.now() > cuenta.latest_withdraw_time+(86400*1000) && recompensas > 0) {
-		console.log("[Reclamando recompensa: "+(Date.now() > cuenta.latest_withdraw_time+86400000)+"]");
-		const tradeobj = await tronWeb.transactionBuilder.withdrawBlockRewards(cuenta.address, 1);
-		const signedtxn = await tronWeb.trx.sign(tradeobj, PEKEY);
-		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-		console.log("[Transaccion: "+receipt.txid+"]");
-	}
-
-	await delay(3000)
-
-	var trx = await fetch("https://apilist.tronscanapi.com/api/account/tokens?address=TWVVi4x2QNhRJyhqa7qrwM4aSXnXoUDDwY&start=0&limit=20&token=trx&hidden=0&show=0&sortType=0").then((r)=>{return r.json()}).catch(console.error)
-	trx = trx.data[0]
-
-	const contractPool = await tronWeb.contract().at(addressContractPool);
-	var trxContract = (await contractPool.TRON_BALANCE().call()).toNumber()/10**6;
-	var trxContractRetiros = (await contractPool.TRON_PAY_BALANCE().call()).toNumber()/10**6;
-	
-	console.log("----------------EJECUCIÓN--------------");
-	console.log("Wallet: "+cuenta.wallet);
-
-	console.log("Disponible: "+cuenta.balance+" TRX");
-	console.log("Congelado: "+(trx.amount-trx.quantity)+" TRX");
-
-	var total = (parseFloat(trx.amount)+trxContractRetiros)
-	console.log("Total: "+total+" TRX");
-	console.log("Registro en Contrato: "+trxContract+" TRX");
-	console.log("Contrato-retiros: "+trxContractRetiros+" TRX")
-
-	var diferencia = (total-trxContract).toFixed(6)
-	console.log("Diferencia: "+diferencia+" TRX");
-
-	console.log("------------------------------");
-
-	var tolerancia = 1; // 1 TRX
-
-	// ajusta las ganancias
-	if(diferencia > tolerancia && true){
-		var tx = await contractPool.gananciaDirecta(parseInt(diferencia*10**6)).send().catch((err)=>{console.log(err)});
-		console.log("[Ejecución: ganancia directa ("+diferencia+") "+tx+"]");
-	}
-
-	// ajusta las perdidas
-	if(diferencia*-1 > tolerancia && true){
-		diferencia = diferencia * -1;
-
-		let calculo = parseInt(diferencia*10**6);
-		let tx = await contractPool.asignarPerdida(calculo).send().catch((err)=>{console.log(err)});
-		console.log("[Ejecución: Ajuste diferencia Negativa (-"+diferencia+") -> "+calculo+" | "+tx+" ]");
-	}
-
-};
-
-async function calculoBRST(){
-	var cuenta = await tronWeb.trx.getAccount();
-	cuenta.wallet = tronWeb.address.fromHex(cuenta.address);
-
-	var recompensas = await tronWeb.trx.getReward(cuenta.address);
-	recompensas = new BigNumber(recompensas).shiftedBy(-6);
-
-	var permTiempo = ( Date.now() >= 1704765600000 ) && (Date.now() > cuenta.latest_withdraw_time+(86400*1000))
-	console.log(permTiempo)
-
-	if (true && permTiempo && recompensas > 0) {
-		console.log("[Reclamando recompensa: "+permTiempo+"]");
-		const tradeobj = await tronWeb.transactionBuilder.withdrawBlockRewards(cuenta.address, 1);
-		const signedtxn = await tronWeb.trx.sign(tradeobj);
-		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
-		console.log("[Transaccion: https://tronscan.io/#/transaction/"+receipt.txid+"]");
-	}
-
-	await delay(3000)
-
-	var balance = await tronWeb3.trx.getUnconfirmedBalance(WALLET_SR);
-	balance = new BigNumber(balance).shiftedBy(-6);
-	
-	var account = await tronWeb3.trx.getAccount()
-	account.wallet = tronWeb.address.fromHex(account.address);
-
-	var trx = await fetch("https://apilist.tronscanapi.com/api/account/tokens?address="+WALLET_SR+"&start=0&limit=20&token=trx&hidden=0&show=0&sortType=0")
-	.then((r)=>{return r.json()})
-	.then((r)=>{return r.data[0]})
-	.catch((e)=>{console.error(e); return false})
-
-	if(trx){
-		//console.log(trx)
-
-		var trxContract = (await contract_Pool.TRON_BALANCE().call()).toNumber()/10**6;
-		var trxContractRetiros = (await contract_Pool.TRON_PAY_BALANCE().call()).toNumber()/10**6;
-
-		var trxContractV4 = (await contract_POOL_PROXY.TRON_BALANCE().call()).toNumber()/10**6;
-		var trxContractRetirosV4 = await tronWeb3.trx.getUnconfirmedBalance(contract_POOL_PROXY.address);
-		trxContractRetirosV4 = new BigNumber(trxContractRetirosV4).shiftedBy(-6);
-		
-		console.log("-------------- EJECUCIÓN V4 ------------");
-		console.log("Ejecutor: "+account.wallet);
-		console.log("Wallet SR: "+cuenta.wallet);
-		console.log(" ")
-		console.log("Disponible: "+balance+" TRX");
-		console.log("Congelado: "+(trx.amount-trx.quantity)+" TRX");
-		console.log(" ")
-		console.log("Para retiros v3: "+trxContractRetiros+" TRX")
-		console.log("Para retiros v4: "+trxContractRetirosV4+" TRX")
-		console.log(" ")
-		var total = (parseFloat(trx.amount)+parseFloat(trxContractRetiros)+parseFloat(trxContractRetirosV4))
-		console.log("Total: "+total+" TRX");
-		console.log(" ")
-		console.log("Registro en Contrato V3: "+trxContract+" TRX");
-		console.log("Registro en Contrato V4: "+trxContractV4+" TRX");
-		console.log(" ")
-
-		var diferencia = (total-trxContract).toFixed(6)
-		console.log("Diferencia V3: "+diferencia+" TRX");
-		console.log(" ")
-		var diferenciaV4 = (total-trxContractV4).toFixed(6)
-		console.log("Diferencia V4: "+diferenciaV4+" TRX");
-
-		console.log("------------------------------");
-
-		var tolerancia = 1; // 1 TRX
-
-		// ajusta las ganancias
-		if(diferenciaV4 > tolerancia && true){
-			await contract_POOL_PROXY.gananciaDirecta(parseInt(diferenciaV4*10**6)).send()
-			.then((h)=>{
-				console.log("[Ejecución: ganancia directa ("+diferenciaV4+") "+h+"]");
-
-			})
-			.catch((err)=>{console.log(err)});
-		}
-
-		// ajusta las perdidas
-		if(diferenciaV4*-1 > tolerancia && true){
-			diferenciaV4 = diferenciaV4 * -1;
-
-			let calculo = parseInt(diferenciaV4*10**6);
-			await contract_POOL_PROXY.asignarPerdida(calculo).send()
-			.then((h)=>{
-				console.log("[Ejecución: Ajuste diferencia Negativa (-"+diferenciaV4+") -> "+calculo+" | "+h+" ]");
-			})
-			.catch((err)=>{console.log(err)})
-			
-		}
-	}else{
-		console.log("error consulta trx")
-	}
 
 };
 
@@ -566,6 +292,94 @@ async function precioBRUT(){
 
 		return {precio: precio, Pricetrx: Pricetrx, variacion: variacion };
 }
+
+async function calculoBRST(){
+	var cuenta = await tronWeb.trx.getAccount();
+	cuenta.wallet = tronWeb.address.fromHex(cuenta.address);
+
+	var recompensas = await tronWeb.trx.getReward(cuenta.address);
+	recompensas = new BigNumber(recompensas).shiftedBy(-6);
+
+	var permTiempo = ( Date.now() >= 1704765600000 ) && (Date.now() > cuenta.latest_withdraw_time+(86400*1000))
+	console.log(permTiempo)
+
+	if (true && permTiempo && recompensas > 0) {
+		console.log("[Reclamando recompensa: "+permTiempo+"]");
+		const tradeobj = await tronWeb.transactionBuilder.withdrawBlockRewards(cuenta.address, 1);
+		const signedtxn = await tronWeb.trx.sign(tradeobj);
+		const receipt = await tronWeb.trx.sendRawTransaction(signedtxn);
+		console.log("[Transaccion: https://tronscan.io/#/transaction/"+receipt.txid+"]");
+	}
+
+	await delay(3000)
+
+	var balance = await tronWeb3.trx.getUnconfirmedBalance(WALLET_SR);
+	balance = new BigNumber(balance).shiftedBy(-6);
+	
+	var account = await tronWeb3.trx.getAccount()
+	account.wallet = tronWeb.address.fromHex(account.address);
+
+	var trx = await fetch("https://apilist.tronscanapi.com/api/account/tokens?address="+WALLET_SR+"&start=0&limit=20&token=trx&hidden=0&show=0&sortType=0")
+	.then((r)=>{return r.json()})
+	.then((r)=>{return r.data[0]})
+	.catch((e)=>{console.error(e); return false})
+
+	if(trx){
+		//console.log(trx)
+
+		var trxContractV4 = (await contract_POOL_PROXY.TRON_BALANCE().call()).toNumber()/10**6;
+		var trxContractRetirosV4 = await tronWeb3.trx.getUnconfirmedBalance(contract_POOL_PROXY.address);
+		trxContractRetirosV4 = new BigNumber(trxContractRetirosV4).shiftedBy(-6);
+		
+		console.log("-------------- EJECUCIÓN V4 ------------");
+		console.log("Ejecutor: "+account.wallet);
+		console.log("Wallet SR: "+cuenta.wallet);
+		console.log(" ")
+		console.log("Disponible: "+balance+" TRX");
+		console.log("Congelado: "+(trx.amount-trx.quantity)+" TRX");
+		console.log(" ")
+		console.log("Para retiros v4: "+trxContractRetirosV4+" TRX")
+		console.log(" ")
+		var total = (parseFloat(trx.amount)+parseFloat(trxContractRetirosV4))
+		console.log("Total: "+total+" TRX");
+		console.log(" ")
+		console.log("Registro en Contrato V4: "+trxContractV4+" TRX");
+		console.log(" ")
+
+		var diferenciaV4 = (total-trxContractV4).toFixed(6)
+		console.log("Diferencia V4: "+diferenciaV4+" TRX");
+
+		console.log("------------------------------");
+
+		var tolerancia = 1; // 1 TRX
+
+		// ajusta las ganancias
+		if(diferenciaV4 > tolerancia && true){
+			await contract_POOL_PROXY.gananciaDirecta(parseInt(diferenciaV4*10**6)).send()
+			.then((h)=>{
+				console.log("[Ejecución: ganancia directa ("+diferenciaV4+") "+h+"]");
+
+			})
+			.catch((err)=>{console.log(err)});
+		}
+
+		// ajusta las perdidas
+		if(diferenciaV4*-1 > tolerancia && true){
+			diferenciaV4 = diferenciaV4 * -1;
+
+			let calculo = parseInt(diferenciaV4*10**6);
+			await contract_POOL_PROXY.asignarPerdida(calculo).send()
+			.then((h)=>{
+				console.log("[Ejecución: Ajuste diferencia Negativa (-"+diferenciaV4+") -> "+calculo+" | "+h+" ]");
+			})
+			.catch((err)=>{console.log(err)})
+			
+		}
+	}else{
+		console.log("error consulta trx")
+	}
+
+};
 
 async function precioBRST(){
 	
